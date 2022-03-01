1678fa40fdc188f7113d6ae59850f6b095c78140
hitenvats16 <hitenvats16@gmail.com>
2022-03-01 11:15:50 +0530

    added

--- src/pages/sign.jsx
+++ src/pages/sign.jsx
@@ -27,8 +27,8 @@ export default function Sign() {
                 />
             </div>
             <div className='w-[400px] h-max py-3 flex justify-between items-center'>
-                <button className='text-transparent bg-clip-text bg-gradient-to-r from-purple-600 to-pink-700 w-1/2 border-4 font-bold mr-1 border-purple-600 py-3 rounded-md'>Sign in</button>
-                <button className='lg:flex font-semibold text-white w-1/2 py-4 justify-center bg-slate-600 rounded-md bg-gradient-to-r ml-1 from-[#9625b8] to-[#d307b1]' >Sign In</button>
+                <button className='text-transparent bg-clip-text bg-gradient-to-r from-purple-600 to-pink-700 w-1/2 border-4 font-bold mr-1 border-purple-600 py-3 rounded-md'>SignUp</button>
+                <button className='lg:flex font-semibold text-white w-1/2 py-4 justify-center bg-slate-600 rounded-md bg-gradient-to-r ml-1 from-[#9625b8] to-[#d307b1]' >SignIn</button>
             </div>
                 <button className='w-[400px] text-center text-purple-500 py-5' onClick={()=>{ nav(-1); }}>Go Back</button>
         </div>
