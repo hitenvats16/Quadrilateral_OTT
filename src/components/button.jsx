abb0f79d349113ce23dfa5f41cb2f0be74bf4724
hitenvats16 <hitenvats16@gmail.com>
2022-03-01 13:42:17 +0530

    added

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -1,10 +1,10 @@
 import '../index.css'
-import {AiOutlineArrowRight} from 'react-icons/ai'
+import {IoMdPlay} from 'react-icons/io'
 const Btn = ()=> {
     return(
         <div className='btn flex flex-row items-center w-36 justify-between px-3 py-2 border-2 border-[#8a39e1] rounded-md mt-5'>
+                <IoMdPlay color='#8a39e1'/>
                 <h1 className='text-[#8a39e1] original_font'>Watch Now</h1>
-                <AiOutlineArrowRight color='#8a39e1'/>
         </div>
     );
 }
